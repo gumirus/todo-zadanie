a6fb35be4e27a06be44a8278c89891a1e86e2c17
Ruslan Gumirov <gumirus29@gmail.com>
2025-01-26 01:52:54 +0500

    fix add build in vite.config.t

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(
-. base: '/dodo-zadanie/',
+export default defineConfig({
+  base: '/dodo-zadanie/',
   plugins: [react()],
   build: {
     outDir: 'dist',
